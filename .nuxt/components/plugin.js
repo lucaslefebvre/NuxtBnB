7110a1ea0def21f01e04a6808cb8bd18a740fb09
Lucas Lefebvre <lucas.lefebvre.pro310@gmail.com>
2021-03-04 17:22:02 +0100

    Routing setup and GoogleMaps API homemade plugin configured

---
+++ .nuxt/components/plugin.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+
+const components = {
+  HomeCard: () => import('../../components/HomeCard.vue' /* webpackChunkName: "components/home-card" */).then(c => c.default || c)
+}
+
+for (const name in components) {
+  Vue.component(name, components[name])
+  Vue.component('Lazy' + name, components[name])
+}
