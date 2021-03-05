10d174d1e0fdf3e57dd17043d1fb58f6bf98933e
Lucas Lefebvre <lucas.lefebvre.pro310@gmail.com>
2021-03-05 14:01:15 +0100

    Factoring API calls for speed

--- plugins/dataApi.js
+++ plugins/dataApi.js
@@ -8,10 +8,26 @@ export default function(context, inject){
     inject('dataApi', {
         getHome,
         getReviewsByHomeId,
+        getUserByHomeId,
     })
     async function getHome(homeId){
         try{
         return unWrap(await fetch(`https://${appId}-dsn.algolia.net/1/indexes/homes/${homeId}`, { headers }))
+        } catch(error){
+            return getErrorResponse(error)
+        }
+    }
+
+    async function getUserByHomeId(homeId){
+        try{
+            return unWrap(await fetch(`https://${appId}-dsn.algolia.net/1/indexes/users/query`, {
+                headers,
+                method: 'POST',
+                body: JSON.stringify({
+                    filters: `homeId:${homeId}`,
+                    attributesToHighlight: [],
+                })
+            }))
         } catch(error){
             return getErrorResponse(error)
         }
